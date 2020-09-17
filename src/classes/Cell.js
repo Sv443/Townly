c0cb2277a779a2fb1274c692a221a9a09f639e12
Sven <s.fehler@pbsnetwork.eu>
2020-09-17 11:58:53 +0200

    added some methods to the Cell class

--- src/classes/Cell.js
+++ src/classes/Cell.js
@@ -8,6 +8,18 @@ const scl = require("svcorelib");
  * @prop {CellType} type
  */
 
+/**
+ * @typedef {Object} ColorsObjectEsc
+ * @prop {String} fg
+ * @prop {String} bg
+ */
+
+/**
+ * @typedef {Object} ColorsObject
+ * @prop {Color} fg
+ * @prop {Color} bg
+ */
+
 class Cell
 {
     /**
@@ -18,6 +30,7 @@ class Cell
     {
         this.type = type;
         this.cursorActive = false;
+        this.char = "?";
     }
 
     /**
@@ -30,20 +43,6 @@ class Cell
             active = false;
 
         this.cursorActive = active;
-    }
-
-    /**
-     * Sets the color of a cell
-     * @param {Color} [fgcolor]
-     * @param {Color} [bgcolor]
-     */
-    setColor(fgcolor, bgcolor)
-    {
-        if(fgcolor)
-            this.fgcolor = fgcolor;
-
-        if(bgcolor)
-            this.bgcolor = bgcolor;
     }
 
     /**
@@ -77,6 +76,59 @@ class Cell
             default:
                 return col.rst;
         }
+    }
+
+    /**
+     * Returns an object containing the colors of the cell
+     * @param {Boolean} escapeCode Set to `false` to get the color name instead of the escape code
+     * @returns {ColorsObject|ColorsObjectEsc}
+     */
+    getColors(escapeCode = true)
+    {
+        if(escapeCode !== false)
+            escapeCode = true;
+
+        return {
+            fg: escapeCode ? this.colorToEscapeCode(this.fgcolor) : this.fgcolor,
+            bg: escapeCode ? this.colorToEscapeCode(this.bgcolor) : this.bgcolor
+        }
+    }
+
+    /**
+     * Sets the colors of a cell
+     * @param {Color} [fgcolor] Leave undefined or falsy to not overwrite the current color
+     * @param {Color} [bgcolor] Leave undefined or falsy to not overwrite the current color
+     */
+    setColors(fgcolor, bgcolor)
+    {
+        if(fgcolor)
+            this.fgcolor = fgcolor;
+
+        if(bgcolor)
+            this.bgcolor = bgcolor;
+    }
+
+    /**
+     * Returns the character of this cell
+     * @returns {String}
+     */
+    getChar()
+    {
+        if(typeof this.char != "string" || this.char.length != 1)
+            throw new Error(`Cell's character is not a string or the length is larger or smaller than 1`);
+    }
+
+    /**
+     * Sets this cell's character
+     * @param {String} char String with a length of 1
+     * @throws Throws an error if the passed parameter is not a string or longer than 1
+     */
+    setChar(char)
+    {
+        if(typeof char != "string" || char.length != 1)
+            throw new Error(`Couldn't set Cell's character: parameter is not a string or the length is larger or smaller than 1`);
+        
+        this.char = char;
     }
 }
 
