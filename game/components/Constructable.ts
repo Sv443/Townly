56551111f5e9ad984a2a139560beeb612a36a0d1
Sv443 <sven.fehler@web.de>
2021-03-31 20:49:12 +0200

    added Constructable and InhabitedCell

---
+++ game/components/Constructable.ts
@@ -0,0 +1,41 @@
+import { DeepPartial } from "tsdef";
+
+import { Position } from "../../engine/base/Base";
+import { CellType, TownlyCell } from "./TownlyCell";
+
+
+//#MARKER types
+
+export interface IConstructableCost
+{
+    [index: string]: number | undefined;
+
+    /** The base cost for this Constructable */
+    baseCost: number;
+    /** The cost will multiply by this factor for every Constructable that already exists - set to `1.0` to disable, less than `1.0` to make it decrement */
+    costIncrement: number;
+}
+
+const defaultIConstructableCost: IConstructableCost = {
+    baseCost: 1000,
+    costIncrement: 1.0
+};
+
+//#MARKER class
+
+/**
+ * A Constructable is any cell that can be constructed by the player.  
+ * They usually cost a certain amount of money.
+ */
+export abstract class Constructable extends TownlyCell
+{
+    cost: IConstructableCost;
+
+
+    constructor(cellType: CellType, pos: Position, char: string, cost: DeepPartial<IConstructableCost>)
+    {
+        super(cellType, pos, char);
+
+        this.cost = { ...defaultIConstructableCost, ...cost };
+    }
+}
---
+++ game/components/InhabitedCell.ts
@@ -0,0 +1,48 @@
+import { DeepPartial } from "tsdef";
+
+import { Position } from "../../engine/base/Base";
+import { Constructable, IConstructableCost } from "./Constructable";
+import { CellType } from "./TownlyCell";
+
+
+//#MARKER types
+
+export interface IInhabitants
+{
+    [index: string]: number | object | undefined;
+
+    /** Max amount of inhabitants that this cell allows */
+    max: {
+        /** The base amount of residents that can live in this cell */
+        residents: number;
+        /** The base amount of workers that can work in this cell */
+        workers: number;
+    }
+}
+
+const defaultIInhabitants: IInhabitants = {
+    max: {
+        residents: 0,
+        workers: 0
+    }
+};
+
+//#MARKER class
+
+/**
+ * A Constructable is any cell that can be constructed by the player.  
+ * They usually cost a certain amount of money.
+ */
+export abstract class InhabitedCell extends Constructable
+{
+    inhabitants: DeepPartial<IInhabitants>;
+
+
+    constructor(cellType: CellType, pos: Position, char: string, cost: DeepPartial<IConstructableCost>, inhabitants: DeepPartial<IInhabitants>)
+    {
+        super(cellType, pos, char, cost);
+
+
+        this.inhabitants = { ...defaultIInhabitants, ...inhabitants };
+    }
+}
